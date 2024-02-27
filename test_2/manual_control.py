309db8310960b30f9685f70880919c9d22ed73ae
Iftach Naftaly <iftahnaf@gmail.com>
2024-02-27 20:27:42 +0000

    fix: lane detection using carla.Waypoint

--- test_2/manual_control.py
+++ test_2/manual_control.py
@@ -143,7 +143,6 @@ try:
 except ImportError:
     raise RuntimeError('cannot import numpy, make sure numpy package is installed')
 
-from lane_detector import LaneDetector
 import cv2
 
 # ==============================================================================
@@ -658,6 +657,76 @@ class KeyboardControl(object):
     @staticmethod
     def _is_quit_shortcut(key):
         return (key == K_ESCAPE) or (key == K_q and pygame.key.get_mods() & KMOD_CTRL)
+    
+# ==============================================================================
+# -- Lane Detector -------------------------------------------------------------
+# ==============================================================================
+
+# based on https://github.com/carla-simulator/carla/issues/1254 and https://carla.readthedocs.io/en/latest/python_api/#carla.Waypoint
+
+class LaneDetector(object):
+    def __init__(self, world, current_map, vehicle, camera, camera_bp):
+        self.world = world
+        self.vehicle = vehicle
+        self.map = current_map
+        self.camera = camera
+
+        self.image_w = camera_bp.get_attribute("image_size_x").as_int()
+        self.image_h = camera_bp.get_attribute("image_size_y").as_int()
+        self.fov = camera_bp.get_attribute("fov").as_float()
+
+        self.K = self.build_projection_matrix(self.image_w, self.image_h, self.fov)
+        self.world_2_camera = np.array(self.camera.get_transform().get_inverse_matrix())
+
+    def detect(self):        
+        location = self.vehicle.get_location()
+        nearest_waypoint = self.map.get_waypoint(location, project_to_road=True)
+
+        left_lane_waypoint =  nearest_waypoint.get_left_lane()
+        right_lane_waypoint = nearest_waypoint.get_right_lane()
+
+        left_lane_location = left_lane_waypoint.transform.location
+        right_lane_location = right_lane_waypoint.transform.location
+
+        left_lane_point = self.get_image_point(left_lane_location, self.K, self.world_2_camera)
+        right_lane_point = self.get_image_point(right_lane_location, self.K, self.world_2_camera)
+
+        print(left_lane_point, right_lane_point)
+
+        # return left_lane_point, right_lane_point
+
+    @staticmethod
+    def get_image_point(loc, K, w2c):
+        # Calculate 2D projection of 3D coordinate
+
+        # Format the input coordinate (loc is a carla.Position object)
+        point = np.array([loc.x, loc.y, loc.z, 1])
+        # transform to camera coordinates
+        point_camera = np.dot(w2c, point)
+
+        # New we must change from UE4's coordinate system to an "standard"
+        # (x, y ,z) -> (y, -z, x)
+        # and we remove the fourth componebonent also
+        point_camera = [point_camera[1], -point_camera[2], point_camera[0]]
+
+        # now project 3D->2D using the camera matrix
+        point_img = np.dot(K, point_camera)
+        # normalize
+        point_img[0] /= point_img[2]
+        point_img[1] /= point_img[2]
+
+        return point_img[0:2]
+    
+    @staticmethod
+    def build_projection_matrix(w, h, fov):
+        focal = w / (2.0 * np.tan(fov * np.pi / 360.0))
+        K = np.identity(3)
+        K[0, 0] = K[1, 1] = focal
+        K[0, 2] = w / 2.0
+        K[1, 2] = h / 2.0
+        return K
+
+
 
 
 # ==============================================================================
